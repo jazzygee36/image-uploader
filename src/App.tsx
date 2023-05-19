e0470d97368bdd3d7959b59ded89b63a9fd616c2
unknown <jazzygee36gmail.com>
2023-05-20 00:21:48 +0100

    image-uploader

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Modal } from "antd";
+
+function App() {
+  return (
+    <div className="main-container">
+      <div className="image-header">
+        <h1>Upload your image</h1>
+        <p>File should be Jpeg, Png</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
